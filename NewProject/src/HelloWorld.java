5395592f6448789e6ce54c8a8603c1cec6d78a13
Ankhaa <Ankhaa@Ankhaa0232>
2024-01-26 21:46:16 +0800

    commit2.

--- NewProject/src/HelloWorld.java
+++ NewProject/src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Result: " +func(2, 3));
+		System.out.println("Result: " +func(2, 4));
 	}
 	public static int func( int c, int x) {
 		return c+x;
