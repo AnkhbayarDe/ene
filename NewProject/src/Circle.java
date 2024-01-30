6b7cfa4c91b8be1e53e04f3c5b2d9efad2c369e8
Ankhaa <Ankhaa@Ankhaa0232>
2024-01-30 14:13:34 +0800

    new

--- NewProject/src/Circle.java
+++ NewProject/src/Circle.java
@@ -7,23 +7,29 @@ public Circle(double radius) {
 this.radius = radius;
 }
 public static void main(String[] args) {
-	// int R = 3;
+	 int R;
 	
 		System.out.println("------------------------------------------------------------");
 		System.out.println("CircleCalc v1.0");
-		System.out.println();
-		System.out.println("");
 		System.out.println("------------------------------------------------------------");
-		System.out.print("Enter the circle's radius: ");
-		Scanner R = new Scanner(System.in);
+		System.out.println("");
+		 int min = 1; 
+	     int max = 50; 
+	      
+		 R = (int)Math.floor(Math.random() * (max - min + 1) + min);
+		 System.out.println(R);
+		//Scanner R = new Scanner(System.in);
 		//Circle c = new Circle.nextFloat());
-
-}
-
-public static double Area(int R) {
+		 System.out.println("");
+		 System.out.println(Area(R));
+		 System.out.println("");	
+		}
+		
+		
+	public static double Area(int R) {
 	double S;
 	S = PI*R*R;
-	System.out.println("Area of Circle" +S);
+	System.out.println("Area of Circle: " +S);
 	
 	return S;
 }
---
+++ NewProject/src/Circle2.java
@@ -0,0 +1,14 @@
+
+public class Circle2 {
+
+	public static void main(String[] args) {
+	     
+		
+		  int min = 1; 
+	      int max = 50; 
+	      
+		 int random_int = (int)Math.floor(Math.random() * (max - min + 1) + min);
+		 System.out.println(random_int);
+	}
+
+}
