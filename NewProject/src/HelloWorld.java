e2bd142dbf38bbbecc66a94264ef665e2bd40ae4
Ankhaa <Ankhaa@Ankhaa0232>
2024-01-30 12:32:04 +0800

    version 2.0

--- NewProject/src/HelloWorld.java
+++ NewProject/src/HelloWorld.java
@@ -2,7 +2,8 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Result: " +func(2, 4));
+		System.out.println("Result: " +func(8, 4));
+		System.out.println("Ene haragdaj baina uu");
 	}
 	public static int func( int c, int x) {
 		return c+x;
