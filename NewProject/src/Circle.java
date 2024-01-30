f1b89b2c0036d11562f13dc2d15bb0d54c097df9
Ankhaa <Ankhaa@Ankhaa0232>
2024-01-30 13:26:59 +0800

    added area function

--- NewProject/src/Circle.java
+++ NewProject/src/Circle.java
@@ -2,18 +2,47 @@ import java.util.Scanner;
 public class Circle {
 private static final double PI = 3.14;
 private final double radius;
+
 public Circle(double radius) {
 this.radius = radius;
 }
 public static void main(String[] args) {
-System.out.println("------------------------------------------------------------");
-System.out.println("CircleCalc v1.0");
-System.out.println();
-System.out.println("Calculates and prints information for a user-supplied radius");
-System.out.println("------------------------------------------------------------");
-System.out.print("Enter the circle's radius: ");
-Scanner in = new Scanner(System.in);
-Circle c = new Circle(in.nextFloat());
-// TODO: compute and print the circumference and area of the circle
+	// int R = 3;
+	
+		System.out.println("------------------------------------------------------------");
+		System.out.println("CircleCalc v1.0");
+		System.out.println();
+		System.out.println("Calculates and prints information for a user-supplied radius");
+		System.out.println("------------------------------------------------------------");
+		System.out.print("Enter the circle's radius: ");
+		Scanner R = new Scanner(System.in);
+		Circle c = new Circle.nextFloat());
+
+}
+
+public static double Area(int R) {
+	double S;
+	S = PI*R*R;
+	System.out.println("Area of Circle" +S);
+	
+	return S;
 }
 }
+
+/*
+
+1. Start
+
+2. Input the the value of radius R
+
+3. Let PI=3.14
+
+4. Calculate area=PI*R*R
+
+5. Print area
+
+6.En
+
+
+
+*/
