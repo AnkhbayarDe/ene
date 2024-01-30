047844a4743657e173d6b8cbb9df07a82583baf2
Ankhaa <Ankhaa@Ankhaa0232>
2024-01-30 13:28:55 +0800

    added Area function

--- NewProject/src/Circle.java
+++ NewProject/src/Circle.java
@@ -16,7 +16,7 @@ public static void main(String[] args) {
 		System.out.println("------------------------------------------------------------");
 		System.out.print("Enter the circle's radius: ");
 		Scanner R = new Scanner(System.in);
-		Circle c = new Circle.nextFloat());
+		//Circle c = new Circle.nextFloat());
 
 }
 
